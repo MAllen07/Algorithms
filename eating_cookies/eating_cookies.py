064bd762ef88999aec81d9f6a0ca82569ace6ec5
Mindy Allen <mrsmallen07@gmail.com>
2019-10-11 01:18:03 -0400

    test passed

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,29 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+    if n < 0:
+      return 0
+    elif n == 0:
+      return 1
+
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+cache = {
+    0: 0,
+    1: 1
+}
+
+def cookie_cache(n):
+    global cache
+
+    if n in cache:
+        return cache[n]
+
+    cache[n] = cookie_cache(n-1) + cookie_cache(n-2) + cookie_cache(n-3)
+
+    return cache[n]
+
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
